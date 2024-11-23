48108d4e7f78bea9bd3364b420da8e98c67de08d
Bhaven Rathod <rathodbhavendilip@gmail.com>
2024-11-23 08:23:33 +0530

    count occurences done

---
+++ 08 - Binary Search/BS on 1D arrays/Count Occurrences in Sorted Array/bruteForce.cpp
@@ -0,0 +1,28 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+int count(vector<int> &arr, int n, int x)
+{
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+
+        // counting the occurrences:
+        if (arr[i] == x)
+            cnt++;
+    }
+    return cnt;
+}
+
+// TC is O(n)
+// SC is O(1)
+
+int main()
+{
+    vector<int> arr = {2, 4, 6, 8, 8, 8, 11, 13};
+    int n = 8, x = 8;
+    int ans = count(arr, n, x);
+    cout << "The number of occurrences is: "
+         << ans << "\n";
+    return 0;
+}
