121f038ac489961d85eaecde78f2ff91daca06f2
Bhaven Rathod <rathodbhavendilip@gmail.com>
2025-01-06 10:47:07 +0530

    bruteForce done

---
+++ 08 - Binary Search/BS on 1D arrays/Single element in sorted array/bruteForce.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findSingle(vector<int> &arr, int size)
+{
+    if (size == 1)
+        return arr[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (i == 0)
+        {
+            if (arr[i] != arr[i + 1])
+                return arr[i];
+        }
+        else if (i == size - 1)
+        {
+            if (arr[i] != arr[i - 1])
+                return arr[i];
+        }
+        else
+        {
+            if (arr[i] != arr[i - 1] && arr[i] != arr[i + 1])
+                return arr[i];
+        }
+    }
+}
+
+// TC is O(n)
+// SC is O(1)
+
+int main()
+{
+    vector<int> arr = {1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6};
+    int n = arr.size();
+    int ans = findSingle(arr, n);
+    cout << "The single element is " << ans;
+    return 0;
+}
